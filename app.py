638a625aab8bceb81488201ac8988f2858d1b944
ruk <ruk.lakhani@students.makeschool.com>
2019-11-08 20:50:43 -0800

    updates

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from random import randint 
+import requests
+
 app = Flask(__name__)
+API_KEY='b2a6e5d68873b7da1769e863bcf62229'
 
 @app.route('/')
 def index(): 
@@ -45,6 +48,27 @@ def results():
         'fortune': fortune
     }
     return render_template('results.html', fortunes=fortunes)
+
+@app.route ('/weather')
+def weather():
+    return render_template('weather_form.html')
+
+@app.route('/weather_results')
+def weather_results_page():
+    user_city = request.args.get('city')
+    params = {
+        'appid': API_KEY,
+        'q': user_city, 
+        'units': 'Imperial'
+    }
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather', params)
+    if not r.status_code == 200:
+        print("error")
+        return render_template('weather_form.html')
+    results = r.json()
+    city = results['name']
+    degrees = results['main']['temp']
+    return render_template('weather_results.html', city=city, degrees=degrees)
 
 if __name__ == '__main__':
     app.run()
